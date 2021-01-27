73c392628d01065bcb25540c73d8268bb9ed4db1
Zed Tan <zed@shootbird.work>
2021-01-27 05:12:55 +0100

    move to much simpler usage of xmltodict to get data out of xml file

--- main.py
+++ main.py
@@ -1,69 +1,22 @@
 import pprint
-import xml.etree.ElementTree as ET
+import xmltodict
 
 # https://www.tutorialspoint.com/xml-parsing-in-python
 
-pp = pprint.PrettyPrinter(width=41, compact=True).pprint
+pp = pprint.PrettyPrinter(width=41, compact=True).pformat
 
-# DATA = "_data/sitemap.xml"
-DATA = "_data/test.xml"
+DATA = "_data/sitemap.xml"
+# DATA = "_data/test.xml"
 SCHEMA = '{http://www.sitemaps.org/schemas/sitemap/0.9}'
 
 
-class Elem:
-    def __init__(
-            self,
-            tag: dict,
-            attributes: dict,
-            text: str,
-            ) -> dict:
-
-        self.tag = tag
-        self.attributes = attributes
-        self.text = text
-
-    def to_dict(self):
-        return {
-            "tag": self.tag,
-            "attributes": self.attributes,
-            "text": self.text,
-        }
-
-
-def getXMLtree(filename: str) -> ET.ElementTree:
-    tree = ET.ElementTree(file=filename)
-    return tree
-
-
-def getXMLroot(tree: ET.ElementTree) -> ET.Element:
-    root = tree.getroot()
-    return root
-
-
-def parseElement(elem: ET.Element) -> Elem:
-    return Elem(
-                elem.tag,
-                elem.attrib,
-                elem.text,
-                )
-
-
-def traverselXML(parent: ET.ElementTree):
-    root = parent.getroot()
-    elemList = list()
-    if len(root) and isinstance(root, ET.ElementTree):
-        # apparently we're not parsing children as children?
-        children = root.getchildren()
-        pp(children)
-    else:
-        for elem in root.iter():
-            elemList.append(parseElement(elem).to_dict())
-    return elemList
-
-
 if __name__ == "__main__":
-    tree = getXMLtree(DATA)
-    output = traverselXML(tree)
+    with open(DATA, "r") as f:
+        data = xmltodict.parse(f.read())
 
-    with open("output.log", "w") as file:
-        file.write(f"{output}")
+        url_list = data.get("urlset").get("url")  # type: list
+
+        output = url_list
+
+        with open("output.log", "w") as file:
+            file.write(f"{output}")
