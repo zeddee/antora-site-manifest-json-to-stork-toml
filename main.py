a38f23566ee484e3245b0315c829b363573c1af0
Zed Tan <zed@shootbird.work>
2021-01-29 22:17:23 +0100

    completed initial implementation; now can ingest a site-manifest.json file from antora site built using https://gitlab.com/opendevise/oss/antora-site-generator-ms and produce a stork-friendly toml file

--- main.py
+++ main.py
@@ -9,44 +9,106 @@ pp = pprint.PrettyPrinter(width=41, compact=True).pformat
 DATA = "_data/site-manifest.json"
 
 
-class StorkConfigRaw:
-    def __init__(
-            self,
-            base_directory: str,
-            url_prefix: str,
-            files: str,
-            output_filename: str,
-            debug: bool = False,
-            ):
-        self.base_directory = base_directory
-        self.url_prefix = url_prefix
-        self.files = files
-
-        self.output_filename = output_filename
-        self.debug = debug
-
-
-class StorkFileListEntry:
+class StorkFileItem:
     def __init__(
             self,
             path: str,
             url: str,
             title: str,
             ):
-        self.path = path,
-        self.url = url,
+        self.path = path
+        self.url = url
         self.title = title
+
+    def to_dict(self) -> dict:
+        return {
+            "path": self.path,
+            "url": self.url,
+            "title": self.title,
+        }
+
+
+StorkFileList = list[dict]  # type alias
+
+
+class StorkConfigRaw:
+    def __init__(
+            self,
+            base_directory: str,
+            url_prefix: str,
+            files: StorkFileList,
+            output_filename: str,
+            debug=False,  # type: bool
+            ):
+        self.base_directory = base_directory  # type: str
+        self.url_prefix = url_prefix  # type: str
+        self.files = files  # type: StorkFileList
+
+        self.output_filename = output_filename  # type: str
+        self.debug = debug  # type: bool
+
+
+class StorkTemplate:
+    def toml(c: StorkConfigRaw) -> str:
+        assert(
+            c.base_directory and
+            c.url_prefix and
+            c.files and
+            c.output_filename and
+            isinstance(c.debug, bool)
+        )
+        return f"""# Generated from {__name__}
+
+[input]
+base_directory = \"{c.base_directory}\"
+url_prefix = \"{c.url_prefix}\"
+files = {c.files}
+
+[output]
+filename = \"{c.output_filename}\"
+debug = {c.debug}
+"""
+
+
+def parse_sitmanifest(data: dict) -> StorkConfigRaw:
+    file_list = []
+    for component in data.get("components"):
+        for version in component.get("versions"):
+            for page in version.get("pages"):
+                file_list.append(
+                    StorkFileItem(
+                        page.get("path"),
+                        page.get("url"),
+                        page.get("title")
+                        ).to_dict()
+                )
+
+    return StorkConfigRaw(
+        base_directory="build/site",
+        url_prefix=data.get("url"),
+        files=file_list,
+        output_filename="site-manifest.toml"
+    )
 
 
 if __name__ == "__main__":
-    stork = StorkConfigRaw("basedir", "urlpref", "files", "outfile")
+    stork = StorkConfigRaw(
+        "basedir",
+        "urlpref",
+        "files",
+        "outfile",
+        # True
+        )
 
     with open(DATA, "r") as f:
         data = json.load(f)
         f.close()
 
+    parsed_manifest = parse_sitmanifest(data)
+    output_toml = StorkTemplate.toml(parsed_manifest)
+
     output = toml.dumps(data)
 
     with open("output.log", "w") as file:
-        file.write(f"{output}")
+        file.write(f"{output_toml}")
         file.close()
